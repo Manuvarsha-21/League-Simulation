Fixed probability calculation bug

League_simulation.py
@@ -129,8 +129,17 @@ class League_simulation:
         mc_df = pd.DataFrame(mc_data).sort_values("Win Probability %", ascending=False)
         return mc_df.to_html(index=False)
 
-    
     def run_monte_carlo(self, trials=5000):
+        total_points = sum(team.points for team in self.teamdetails)
+        for team in self.teamdetails:
+            if total_points == 0:
+                team.win_probability = 0
+            else:
+                team.win_probability = round(
+                (team.points / total_points) * 100,
+                1
+                )
+    """def run_monte_carlo(self, trials=5000):
         for a in self.teamdetails:
             for b in self.teamdetails:
                 if a == b:
@@ -141,7 +150,7 @@ class League_simulation:
                     gb = np.random.poisson(max(0.1, b.tar / a.tdr))
                     if ga > gb:
                         wins += 1
-                a.win_probability += round(wins / trials * 100, 1)
+                a.win_probability += round(wins / trials * 100, 1)"""
 
 print("Ready to Simulate League?")
 num=int(input("Enter number of Teams:"))